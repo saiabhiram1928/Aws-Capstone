2e1b94abc2b888543f1c49ef15ba68cfa461a51f
saiabhiram1928 <saiabhiram1928@gmail.com>
2024-10-21 16:54:47 +0530

    BugFix:Database Error ,Added:Bootstrap Script

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+sudo yum update -y 
+sudo yum install git -y
+sudo yum install python3 -y
+sudo yum install python3-pip -y
+cd /home/ec2-user
+sudo git clone https://github.com/saiabhiram1928/Aws-Capstone.git
+cd Aws-Capstone/Aws
+sudo pip3 install -r requirements.txt
+sudo python3 manage.py runserver 0.0.0.0:80 > server_log.txt 2>&1 &
