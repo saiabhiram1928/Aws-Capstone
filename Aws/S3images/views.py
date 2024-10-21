ef3bd65a2d5f6ee725f2cc74969c087917530589
saiabhiram1928 <saiabhiram1928@gmail.com>
2024-10-21 16:19:09 +0530

    First Commit

---
+++ Aws/S3images/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+import boto3
+
+# Create your views here.
+def getImagesFromS3(request):
+    s3 = boto3.client('s3')
+    bucket_name = 'saiabhiram-s3'
+    response = s3.list_objects_v2(Bucket = bucket_name,Prefix='f1')
+    images = []
+    if 'Contents' in response:
+        for obj in response['Contents']:
+            key = obj['Key']
+            if key.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+                image_url = f'https://{bucket_name}.s3.amazonaws.com/{key}'
+                images.append(image_url)
+    return render(request, 's3images/list.html', {'images': images})
+
+
+
+
+
